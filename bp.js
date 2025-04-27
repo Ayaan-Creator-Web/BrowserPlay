829e394f4db11e1534c576490acad32045a12ad1
Ayaan-Creator-Web <ayaan.khalique3@gmail.com>
2025-04-27 21:38:09 +1000

    Add files via upload

---
+++ bp.js
@@ -0,0 +1,68 @@
+function crash() {
+    while (true) {}
+}
+
+const user = JSON.parse(localStorage.getItem('savedUser')) ? JSON.parse(localStorage.getItem('savedUser')) : null;
+
+var visits = localStorage.getItem('visits') ? JSON.parse(localStorage.getItem('visits')) : 0;
+visits += 1;
+localStorage.setItem('visits', JSON.stringify(visits));
+
+function header() {
+    const header = document.querySelector('#text');
+    if (visits > 1) {
+        header.innerText = `Welcome back to Browserplay, ${user.username}!`;
+    }
+    else if (visits == 1) {
+        header.innerText = `Welcome to Browserplay, ${user.username}!`;
+    }
+}
+
+header();
+
+async function closeWindow() {
+    window.close();
+    await delay(1);
+    alert('We are encountering an issue with your browser. Please try again later.');
+}
+
+async function selfDestruct() {
+    const selfDestructTimer = document.querySelector('#text');
+    selfDestructTimer.innerHTML = 'Self-destruct in 5 seconds...';
+    await delay(1000);
+    selfDestructTimer.innerHTML = 'Self-destruct in 4 seconds...';
+    await delay(1000);
+    selfDestructTimer.innerHTML = 'Self-destruct in 3 seconds...';
+    await delay(1000);
+    selfDestructTimer.innerHTML = 'Self-destruct in 2 seconds...';
+    await delay(1000);
+    selfDestructTimer.innerHTML = 'Self-destruct in 1 seconds...';
+    await delay(1000);
+    document.body.innerHTML = '';
+}
+
+function addToHistory() {
+    history.pushState(null, '', 'new-page');
+}
+
+//addToHistory();
+
+function delay(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
+
+var users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : [];
+
+async function checkIfLoggedIn() {
+    if (!user) {
+        //window.location.href = "http://127.0.0.1:5500/Sign-In/Sign-In.html";
+        document.body.innerHTML = '<h1 style="color: red;">You are not logged in. Please log in to access this page.</h1>';
+        //alert('You are not logged in. Please log in to access this page.');
+    }
+}
+
+checkIfLoggedIn();
+
+function shakeButton() {
+    let button = document.getElementById("shakeBtn");
+    button.classList.add("shake");
+    setTimeout(() => button.classList.remove("shake"), 500);
+}
---
+++ signin.js
@@ -0,0 +1,155 @@
+var email
+
+var password
+
+var username
+
+var savedusername = '';
+
+const verifypath = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+//JSON.parse(localStorage.getItem('users'))
+
+let users = [{
+    username: 'Ayaan',
+    email: 'ayaan.khalique3@gmail.com',
+    password: 'sd',
+    visits: JSON.parse(localStorage.getItem('visits')) ? JSON.parse(localStorage.getItem('visits')) : 0,
+}];
+/*
+[{
+    username: 'Ayaan',
+    email: 'ayaan@example.com',
+    password: 'student123'
+}, {
+    username: 'Wasiullah',
+    email: 'wasiullah@example.com',
+    password: 'teacher321'
+}]
+*/
+
+function signin() {
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    username = document.getElementById('username').value
+
+    //fetching updated user from local storage
+    users = users;
+    users.forEach((check) => {
+        //console.log(x);
+        //debugger;
+        if(check.username == username){
+            if (email.match(verifypath)) {
+                if (check.email == email) {
+                    if (check.password == password) {
+                        //alert('Welcome ' + check.username + '!')
+                        savedusername += localStorage.setItem('savedusername', JSON.stringify(username));
+                        const user = users.find(u => u.email === email);
+                        localStorage.setItem('savedUser', JSON.stringify(user));
+                        window.location.href = "http://127.0.0.1:5500/Browserplay/bp.html";
+                        //window.location.href = "https://ayaan-creator-web.github.io/Browserplay/";
+                        savedusername = '';
+                    }
+                }
+                else {
+                    random = Math.random()
+                
+                    if (random < 0.5) {
+                        alert('Please Check Username or Email');
+                    }
+                    else {
+                        alert('Please Check Email or Password');
+                    }
+                }   
+            }
+            else {
+                alert('Please enter valid email')
+            }
+        }
+        else {
+            //alert('Please Check Username or Email');
+        }
+    });
+    //verifyemail(email)
+}
+
+function signup(newId) {
+    email = document.getElementById('email2').value
+    password = document.getElementById('password2').value
+    username = document.getElementById('username2').value
+
+    var newId
+
+    if (users.length == 0) {
+        newId = 0;
+    }
+    else {
+        newId = users.length
+    }
+
+    if (!CheckUserExist(email)) {
+        if (email.match(verifypath)) {
+            if (username && password) {
+                const newuser = {
+                username,
+                email,
+                password,
+                id: newId
+                }
+                users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : []
+                users.push(newuser);
+                alert('Thanks for Signing Up ' + username + '! Please Sign In to Continue')
+                localStorage.setItem('users', JSON.stringify(users));
+
+            }
+            else {
+                alert('Please Fill in Form Completely')
+            }
+        }
+        else {
+            alert('Please Input a Valid Email')
+        }
+    
+    }
+    else {
+        alert('That Email is Already Registered');
+    }
+}
+
+function CheckUserExist(email) {
+
+    //fetching updated user from local storage
+    users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : []
+    let doesExist = false
+    //debugger;
+    users.forEach((check) => {
+       if (check.email == email) {
+        doesExist = true;
+       }
+    });
+    return doesExist;
+}
+
+
+async function forgotPassword() {
+    if (localStorage) {
+        window.location.href="Forgot-Password.html";
+    }
+}
+
+
+//localStorage.clear()
+//johndoe@example.com
+//teacher321
+//student123
+//0187009@schoolsnet.act.edu.au
+
+/*
+when signed in:
+set local storage for login user
+redirect to new page
+do not allow user to paste url in browser
+allow hompage to only allow users
+localStorage.setItem('xyz', 'login successfully');
+say welcome username in alert message
+*/
